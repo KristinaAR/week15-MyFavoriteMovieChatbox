3139529f2ee4e037aa3fc14e59e1cbbde5be5859
Kristina Armus <kristina.armus@tktk.ee>
2024-12-09 09:48:20 +0200

    add getmovie route

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const http = require('http');
 const axios = require('axios');
 
 const app = express();
@@ -6,6 +7,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res)=> {
     let url = 'https://api.themoviedb.org/3/movie/845781?api_key=533f0d499644fb099ce7d287fb7a2553';
@@ -91,6 +93,53 @@ app.post('/search', (reg, res) => {
     }));
    
 });
+
+//new route
+app.post('/getmovie', (req, res) => {
+	const movieToSearch =
+		req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie
+			? req.body.queryResult.parameters.movie
+			: '';
+
+	const reqUrl = encodeURI(
+		`http://www.omdbapi.com/?t=${movieToSearch}&apikey=2d1f8617`
+	);
+	http.get(
+		reqUrl,
+		responseFromAPI => {
+			let completeResponse = ''
+			responseFromAPI.on('data', chunk => {
+				completeResponse += chunk
+			})
+			responseFromAPI.on('end', () => {
+				const movie = JSON.parse(completeResponse);
+                if (!movie || !movie.Title) {
+                    return res.json({
+                        fulfillmentText: 'Sorry, we could not find the movie you are asking for.',
+                        source: 'getmovie'
+                    });
+                }
+
+				let dataToSend = movieToSearch;
+				dataToSend = `${movie.Title} was released in the year ${movie.Year}. It is directed by ${
+					movie.Director
+				} and stars ${movie.Actors}.\n Here some glimpse of the plot: ${movie.Plot}.`;
+
+				return res.json({
+					fulfillmentText: dataToSend,
+					source: 'getmovie'
+				});
+			})
+		},
+		error => {
+			return res.json({
+				fulfillmentText: 'Could not get results at this time',
+				source: 'getmovie'
+			});
+		}
+	)
+});
+
 
 
 app.listen(process.env.PORT || 3000, ()=> {
